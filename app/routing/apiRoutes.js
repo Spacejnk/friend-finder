cc2562d62b38c3b5181f8f0d5020e078fe198481
Timits1 <will4f21@yahoo.com>
2019-09-16 05:05:04 -0700

    format clean up documentation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,78 +1,40 @@
-var friends = require('../data/friends');
+var friends = require("../data/friends");
 
-var path = require('path');
-
-
-
+var path = require("path");
 
 module.exports = function(app) {
-    app.get('api/friends', function(req, res){
-        res.json(friends);
-    });
+  app.get("api/friends", function(req, res) {
+    res.json(friends);
+  });
 
-
-
-
-
-
-        app.post('/api/friends', function(req, res){
-
-
-
+  app.post("/api/friends", function(req, res) {
     var userData = req.body;
-    // var userName = userData.name;
-    // var userPhoto = userData.photo;
-     var userScores = userData.scores;
-    
-        var bestMatch = {
-            name: '',
-            photo: '',
-            friendDifference: 1000
-        };
+    var userScores = userData.scores;
 
-    
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+
     for (let index = 0; index < friends.length; index++) {
-        //const element = array[index]; 
-        
-        totalDifference = 0;
+      totalDifference = 0;
 
-        for (let indexTwo = 0; indexTwo < userScores.length; indexTwo++) {
-            //const element = array[indexTwo];
+      for (let indexTwo = 0; indexTwo < userScores.length; indexTwo++) {
+        totalDifference += Math.abs(
+          friends[index].scores[indexTwo] - userScores[indexTwo]
+        );
 
-            totalDifference += Math.abs(friends[index].scores[indexTwo] - userScores[indexTwo]);
-
-            if (totalDifference <= bestMatch.friendDifference) {
-
-                bestMatch.name = friends[index].name;
-                bestMatch.photo = friends[index].photo;
-                bestMatch.friendDifference = totalDifference;
-
-                
-            }
-
-
-            
+        if (totalDifference <= bestMatch.friendDifference) {
+          bestMatch.name = friends[index].name;
+          bestMatch.photo = friends[index].photo;
+          bestMatch.friendDifference = totalDifference;
         }
-
-
-        
+      }
     }
 
     friends.push(userData);
 
     res.json(bestMatch);
-
-    
-
-});
-
-
-
-
-
-
-
-
+  });
 };
-
-   
